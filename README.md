7cb05a256b93819720c5761e2b4945bbc6d0ceb5
Travis Arnold <ftntravis@gmail.com>
2022-10-23 21:04:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# react cli example
+
+Example showing how to bundle a CLI tool using [react ink](https://github.com/vadimdemedes) and [tsup](https://tsup.egoist.dev/).
+
+## Usage
+
+```bash
+pnpm install && pnpm build && pnpm start
+```
---
+++ index.tsx
@@ -0,0 +1,21 @@
+#!/bin/env node
+import React, { useState, useEffect } from 'react'
+import { render, Text } from 'ink'
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCounter((previousCounter) => previousCounter + 1)
+    }, 100)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  return <Text color="green">{counter} tests passed</Text>
+}
+
+render(<Counter />)
